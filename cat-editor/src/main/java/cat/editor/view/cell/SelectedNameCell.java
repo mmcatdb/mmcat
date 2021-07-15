4870e643e756123aae31aa6f168069ba44280497
Pavel Koupil <contos@ksi.mff.cuni.cz>
2021-07-15 11:57:17 +0200

    edit categorical cells

--- cat-editor/src/main/java/cat/editor/view/cell/SelectedNameCell.java
+++ cat-editor/src/main/java/cat/editor/view/cell/SelectedNameCell.java
@@ -5,6 +5,7 @@
  */
 package cat.editor.view.cell;
 
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -15,27 +16,45 @@ import javafx.scene.text.Text;
  */
 public class SelectedNameCell extends Cell {
 
-    private static final double SIZE = 10;
+	private static final double SIZE = 15;
 
-    public SelectedNameCell(String id, String name, double x, double y) {
-        super(id);
+	public SelectedNameCell(String id, String name, double x, double y) {
+		super(id);
 
-        Text text = new Text(name);
-        text.setFont(Font.font("DejaVu Sans Mono", 20));
-        double height = text.getBoundsInLocal().getHeight();
-//        System.out.println(height + " ::: height");
-        text.relocate(25, -(height / 2 - SIZE));
+		Text text = new Text(name);
+		text.setFont(Font.font("DejaVu Sans Mono", 16));
+		double height = text.getBoundsInLocal().getHeight();
+		text.relocate(30, -(height / 2 - SIZE) - SIZE);
 
-        Circle shape = new Circle(SIZE, SIZE, SIZE);
-        shape.setUserData("aaa");
-        shape.setStroke(CellColors.SELECTED_STROKE_COLOR);
-        shape.setFill(CellColors.NAME_FILL_COLOR);
-        shape.setStrokeWidth(5);
+		relocate(x, y);
 
-        setView(shape);
-        setView(text);
-        relocate(x, y);
-    }
+		Text idText = new Text(id);
+		idText.setFont(Font.font("DejaVu Sans Mono", 12));
+
+		double textWidth = idText.getBoundsInLocal().getWidth();
+		double textHeight = idText.getBoundsInLocal().getHeight();
+
+		double shapeWidth = SIZE * 2;
+		double shapeHeight = SIZE * 2;
+
+		shapeWidth = shapeWidth > textWidth ? shapeWidth : textWidth + 10;
+		shapeHeight = shapeHeight > textHeight ? shapeHeight : textHeight + 10;
+
+		double diffWidth = shapeWidth - textWidth;
+		double diffHeight = shapeHeight - textHeight;
+		idText.relocate(diffWidth / 2, diffHeight / 2);
+
+		Circle shape = new Circle(SIZE, SIZE, SIZE);
+		shape.setUserData("aaa");
+		shape.setStroke(CellColors.SELECTED_STROKE_COLOR);
+		shape.setFill(CellColors.NAME_FILL_COLOR);
+		shape.setStrokeWidth(5);
+
+		setView(shape);
+		setView(text);
+		setView(idText);
+		relocate(x, y);
+
+	}
 
 }
-
--- cat-editor/src/main/java/cat/editor/view/edge/CategoricalEdge.java
+++ cat-editor/src/main/java/cat/editor/view/edge/CategoricalEdge.java
@@ -7,6 +7,8 @@ package cat.editor.view.edge;
 
 import cat.editor.view.cell.Cell;
 import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 /**
  *
@@ -25,13 +27,27 @@ public class CategoricalEdge extends Edge {
 		line = new Line();
 		line.setStrokeWidth(2);
 
-		line.startXProperty().bind(source.layoutXProperty().add(source.getChildren().get(0).getBoundsInParent().getWidth() / 2.0));
-		line.startYProperty().bind(source.layoutYProperty().add(source.getChildren().get(0).getBoundsInParent().getHeight() / 2.0));
+		var startX = source.layoutXProperty().add(source.getChildren().get(0).getBoundsInParent().getWidth() / 2.0);
+		var startY = source.layoutYProperty().add(source.getChildren().get(0).getBoundsInParent().getHeight() / 2.0);
 
-		line.endXProperty().bind(target.layoutXProperty().add(target.getChildren().get(0).getBoundsInParent().getWidth() / 2.0));
-		line.endYProperty().bind(target.layoutYProperty().add(target.getChildren().get(0).getBoundsInParent().getHeight() / 2.0));
+		var endX = target.layoutXProperty().add(target.getChildren().get(0).getBoundsInParent().getWidth() / 2.0);
+		var endY = target.layoutYProperty().add(target.getChildren().get(0).getBoundsInParent().getHeight() / 2.0);
+
+		line.startXProperty().bind(startX);
+		line.startYProperty().bind(startY);
+
+		line.endXProperty().bind(endX);
+		line.endYProperty().bind(endY);
 
 		getChildren().add(line);
+
+		double xOrientation = startX.get() > endX.get() ? 1.0 : -1.0;
+		double yOrientation = startY.get() > endY.get() ? 1.0 : -1.0;
+
+		Text sourceIdText = new Text(source.getCellId());
+		sourceIdText.setFont(Font.font("DejaVu Sans Mono", 12));
+		sourceIdText.relocate(line.getBoundsInLocal().getMinX() + 30 * xOrientation, line.getBoundsInLocal().getMinY() + 30 * yOrientation);
+		getChildren().add(sourceIdText);
 
 	}
 
