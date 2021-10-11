db92666a1e6fbb5b4f92e35719f2c7600f3b7f60
tuaki <jachymb32@gmail.com>
2021-10-11 08:37:35 +0200

    should have been in the previous commit ...

--- mm-cat/wrappers/wrapper-mongodb/src/main/java/cz/cuni/matfyz/wrapperMongodb/MongoDBPullWrapper.java
+++ mm-cat/wrappers/wrapper-mongodb/src/main/java/cz/cuni/matfyz/wrapperMongodb/MongoDBPullWrapper.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cz.cuni.matfyz.wrapperPostgresql;
+package cz.cuni.matfyz.wrapperMongodb;
 
 import cz.cuni.matfyz.abstractwrappers.AbstractPullWrapper;
 import cz.cuni.matfyz.core.mapping.AccessPath;
@@ -12,90 +12,34 @@ import cz.cuni.matfyz.core.RecordData;
 import cz.cuni.matfyz.core.RecordRoot;
 import cz.cuni.matfyz.core.mapping.AccessPathProperty;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import com.mongodb.client.MongoDatabase;
 import java.util.ArrayList;
 
 /**
  *
  * @author jachym.bartik
  */
-public class PostgreSQLPullWrapper implements AbstractPullWrapper
+public class MongoDBPullWrapper implements AbstractPullWrapper
 {
     private ConnectionProvider connectionProvider; // TODO inject?
     
 	public ForestOfRecords pullForest(String selectAll, AccessPath path)
     {
-        ResultSet resultSet = getData(selectAll);
-        if (resultSet == null)
-            return null;
-        
-        try
-        {
-            ForestOfRecords forest = new ForestOfRecords();
-            
-            while (resultSet.next())
-            {
-                ArrayList<RecordData> data = new ArrayList<RecordData>();
-                for (AccessPathProperty property: path.properties())
-                {
-                    String propertyName = "TODO it should be contained in property.name";
-                    String value = resultSet.getString(propertyName);
-                    
-                    // TODO create RecordData from name and value?
-                    data.add(new RecordData());
-                }
-                
-                // TODO add record data to the root of the tree?
-                RecordRoot tree = new RecordRoot();
-                
-                // TODO add tree to the forest?
-            }
-            
-            resultSet.close();
-            
-            return forest;
-        }
-        catch (Exception exception)
-        {
-            System.err.println("Can't get result: " + exception.getMessage());
-        }
+        // MongoCollection<Document> collection = database.getCollection("kindName TODO");
+
+        // TODO how could be selectAll used here?
         
         return null;
     }
 
 	public ForestOfRecords pullForest(String selectAll, AccessPath path, int limit, int offset)
     {
-        String newSelectAll = String.format("%s\nLIMIT %d\nOFFSET %d", selectAll, limit, offset);
-        return pullForest(newSelectAll, path);
-    }
-    
-    private ResultSet getData(String command)
-    {
-        Connection connection = connectionProvider.getConnection();
-        try
-        {
-            connection.setAutoCommit(false);
-
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(command);
-            statement.close();
-            connection.close();
-           
-            return resultSet;
-        }
-        catch (Exception exception)
-        {
-            System.err.println("Can't get result: " + exception.getMessage());
-        }
-        
+        // TODO
         return null;
     }
 }
 
 interface ConnectionProvider
 {
-    Connection getConnection();
+    MongoDatabase getConnection();
 }
