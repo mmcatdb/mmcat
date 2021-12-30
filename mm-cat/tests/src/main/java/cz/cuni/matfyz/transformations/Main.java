3cd9c1e861f5063888048fe58c9c983f9eacfbef
Pavel Koupil <pavel.koupil@matfyz.cuni.cz>
2021-12-30 20:59:03 +0100

    edit logging settings to debug

--- mm-cat/tests/src/main/java/cz/cuni/matfyz/transformations/Main.java
+++ mm-cat/tests/src/main/java/cz/cuni/matfyz/transformations/Main.java
@@ -4,12 +4,18 @@
  */
 package cz.cuni.matfyz.transformations;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author pavel.koupil
  */
 public class Main {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String... args) {
+		LOGGER.info("TEST");
 	}
 }
