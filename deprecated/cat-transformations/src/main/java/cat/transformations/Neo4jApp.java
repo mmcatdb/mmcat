a525108a497f376a61750725b7c2c77a8a806fbd
tuaki <jachymb32@gmail.com>
2022-01-09 21:36:14 +0100

    remove license headers

--- deprecated/cat-transformations/src/main/java/cat/transformations/Neo4jApp.java
+++ deprecated/cat-transformations/src/main/java/cat/transformations/Neo4jApp.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cat.transformations;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.graphdb.Transaction;
