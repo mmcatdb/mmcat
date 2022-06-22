ee8a23128db55b145030d1223583fcb71f47e23c
tuaki <jachymb32@gmail.com>
2022-06-22 21:48:01 +0200

    vite base fixed via env

--- example-ui/vite.config.ts
+++ example-ui/vite.config.ts
@@ -1,24 +1,31 @@
 import { fileURLToPath, URL } from 'url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-        createHtmlPlugin({
-            inject: {
-                data: {
-                    app_version: process.env.npm_package_version
+export default defineConfig(({ command, mode }) => {
+    // Explicitly load the env files because otherwise they would be loaded after the settings (because their location may depend on this settings).
+    const env = loadEnv(mode, process.cwd(), '');
+    const base = env.BASE_PATH || '/';
+
+    return {
+        plugins: [
+            vue(),
+            createHtmlPlugin({
+                inject: {
+                    data: {
+                        app_version: process.env.npm_package_version
+                    }
                 }
+            })
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
             }
-        })
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    }
+        },
+        base
+    };
 });
