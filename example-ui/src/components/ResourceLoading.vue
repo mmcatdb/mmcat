beb22096b3dca6366a7f0d5b7ef85a8ae34304f6
tuaki <jachymb32@gmail.com>
2022-12-06 22:33:15 +0100

    better loading component

--- example-ui/src/components/ResourceLoading.vue
+++ example-ui/src/components/ResourceLoading.vue
@@ -1,8 +1,28 @@
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const LOADING_WAIT_TIME_IN_MILLISECONDS = 400;
+
+export default defineComponent({
+    data() {
+        return {
+            showLoadingMessage: false,
+            timeoutId: undefined as number | undefined
+        };
+    },
+    mounted() {
+        this.timeoutId = setTimeout(() => {
+            this.showLoadingMessage = true;
+        }, LOADING_WAIT_TIME_IN_MILLISECONDS);
+    },
+    unmounted() {
+        clearTimeout(this.timeoutId);
+    }
+});
 </script>
 
 <template>
-    <p>
+    <p v-if="showLoadingMessage">
         Loading ....
     </p>
 </template>
