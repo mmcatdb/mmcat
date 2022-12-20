10d4dafd6fabcc406858931b0c8912bd04f81008
tuaki <jachymb32@gmail.com>
2022-12-20 21:45:54 +0100

    typo

--- server/src/main/java/cz/cuni/matfyz/server/Settings.java
+++ server/src/main/java/cz/cuni/matfyz/server/Settings.java
@@ -1,10 +1,7 @@
 package cz.cuni.matfyz.server;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
