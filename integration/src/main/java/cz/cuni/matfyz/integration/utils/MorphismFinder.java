9be2bdb02fdc7d1412c3495242b55b0f14eeb675
tuaki <jachymb32@gmail.com>
2023-01-02 04:07:20 +0100

    #12 morphism finder cache

--- integration/src/main/java/cz/cuni/matfyz/integration/utils/MorphismFinder.java
+++ integration/src/main/java/cz/cuni/matfyz/integration/utils/MorphismFinder.java
@@ -4,6 +4,9 @@ import cz.cuni.matfyz.core.instance.InstanceCategory;
 import cz.cuni.matfyz.core.instance.InstanceMorphism;
 import cz.cuni.matfyz.core.instance.InstanceObject;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * @author jachym.bartik
  */
@@ -15,7 +18,13 @@ public class MorphismFinder {
         this.category = category;
     }
 
+    private Map<ObjectIriTuple, InstanceMorphism> fromDirectObjectCache = new TreeMap<>();
+
     public InstanceMorphism findDirectFromObject(InstanceObject object, String pimIri) {
+        final var tuple = new ObjectIriTuple(object, pimIri);
+        if (fromDirectObjectCache.containsKey(tuple))
+            return fromDirectObjectCache.get(tuple);
+
         final var matchingMorphisms = category.morphisms().values().stream()
             .filter(InstanceMorphism::isBase) // TODO optimization
             .filter(morphism -> morphism.dom().equals(object))
@@ -25,10 +34,19 @@ public class MorphismFinder {
         if (matchingMorphisms.size() > 1)
             throw new UnsupportedOperationException("Multiple direct morphisms found from object: " + object.key() + " with pim iri: " + pimIri + ".");
         
-        return matchingMorphisms.size() == 1 ? matchingMorphisms.get(0) : null;
+        final var result = matchingMorphisms.size() == 1 ? matchingMorphisms.get(0) : null;
+        fromDirectObjectCache.put(tuple, result);
+
+        return result;
     }
 
+    private Map<ObjectIriTuple, InstanceMorphism> toDirectObjectCache = new TreeMap<>();
+
     public InstanceMorphism findDirectToObject(InstanceObject object, String pimIri) {
+        final var tuple = new ObjectIriTuple(object, pimIri);
+        if (toDirectObjectCache.containsKey(tuple))
+            return toDirectObjectCache.get(tuple);
+
         final var matchingMorphisms = category.morphisms().values().stream()
             .filter(InstanceMorphism::isBase) // TODO optimization
             .filter(morphism -> morphism.cod().equals(object))
@@ -38,13 +56,40 @@ public class MorphismFinder {
         if (matchingMorphisms.size() > 1)
             throw new UnsupportedOperationException("Multiple direct morphisms found to object: " + object.key() + " with pim iri: " + pimIri + ".");
         
-        return matchingMorphisms.size() == 1 ? matchingMorphisms.get(0) : null;
+        final var result = matchingMorphisms.size() == 1 ? matchingMorphisms.get(0) : null;
+        toDirectObjectCache.put(tuple, result);
+
+        return result;
     }
 
-    public InstanceMorphism findFromObject(InstanceObject object, String pimIri) {
-        final var algorithm = new FromObjectIsaSearch(category, object, pimIri);
+    private Map<ObjectIriTuple, InstanceMorphism> fromObjectCache = new TreeMap<>();
 
-        return algorithm.process();
+    public InstanceMorphism findFromObject(InstanceObject object, String pimIri) {
+        final var tuple = new ObjectIriTuple(object, pimIri);
+        if (fromObjectCache.containsKey(tuple))
+            return fromObjectCache.get(tuple);
+
+        final var algorithm = new FromObjectIsaSearch(category, object, pimIri);
+        final var result = algorithm.process();
+        fromObjectCache.put(tuple, result);
+
+        return result;
+    }
+
+    private record ObjectIriTuple(
+        InstanceObject object,
+        String pimIri
+    ) implements Comparable<ObjectIriTuple> {
+
+        @Override
+        public int compareTo(ObjectIriTuple tuple) {
+            int objectResult = object.compareTo(tuple.object);
+            if (objectResult != 0)
+                return objectResult;
+
+            return pimIri.compareTo(tuple.pimIri);
+        }
+
     }
 
 }
