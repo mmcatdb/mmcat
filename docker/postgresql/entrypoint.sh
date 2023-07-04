27db5e505a79bce85024bd3f90744bfe1729e7ff
tuaki <jachymb32@gmail.com>
2023-07-04 18:49:48 +0200

    now?

--- docker/postgresql/entrypoint.sh
+++ docker/postgresql/entrypoint.sh
@@ -2,6 +2,8 @@
 
 # psql "postgresql://${DB_USERNAME}:${DB_PASSWORD}@localhost/${DB_DATABASE}?sslmode=require" -f src/main/resources/createDatabase.sql
 
+# TODO this should be mostly achieved by some app command, like create:databases or similar
+
 psql --username postgres << EOSQL
 CREATE ROLE ${DB_USERNAME} LOGIN PASSWORD '${DB_PASSWORD}';
 CREATE DATABASE ${DB_DATABASE} OWNER ${DB_USERNAME};
@@ -9,11 +11,11 @@ CREATE DATABASE ${DB_DATABASE} OWNER ${DB_USERNAME};
 SET ROLE ${DB_USERNAME};
 \set db_example_username ${DB_EXAMPLE_USERNAME}
 \set db_example_password ${DB_EXAMPLE_PASSWORD}
-\set db_example_postgresql_host ${DB_EXAMPLE_HOST:=mm-postgresql}
+\set db_example_postgresql_host ${DB_EXAMPLE_POSTGRESQL_HOST:=mm-postgresql}
 \set db_example_postgresql_port ${POSTGRESQL_PORT:=5432}
-\set db_example_mongodb_host ${DB_EXAMPLE_HOST:=mm-mongodb}
+\set db_example_mongodb_host ${DB_EXAMPLE_MONGODB_HOST:=mm-mongodb}
 \set db_example_mongodb_port ${MONGODB_PORT:=27017}
-\set db_example_neo4j_host ${DB_EXAMPLE_HOST:=mm-neo4j}
+\set db_example_neo4j_host ${DB_EXAMPLE_NEO4J_HOST:=mm-neo4j}
 \set db_example_neo4j_port ${MM_NEO4J_PORT:=7687}
 \i createDatabase.sql;
 
