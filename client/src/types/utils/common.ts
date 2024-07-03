c3883c28e09c594b5552352b214132a8d02950b9
tuaki <jachymb32@gmail.com>
2024-07-03 11:33:49 +0200

    duplicate fixes from new client

--- client/src/types/utils/common.ts
+++ client/src/types/utils/common.ts
@@ -1,3 +1,7 @@
+// This is kosher because this is the one use case in which the {} type actually means something.
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type EmptyIntersection = {};
+
 export async function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
