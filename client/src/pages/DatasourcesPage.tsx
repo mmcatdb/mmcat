436ab8a899a39102a247c9647196119a23d22aeb
Šárka Mayerová <mayerova.s@email.cz>
2024-08-23 23:10:28 +0200

    Datasource add form template

--- client/src/pages/DatasourcesPage.tsx
+++ client/src/pages/DatasourcesPage.tsx
@@ -1,13 +1,13 @@
 import { CommonPage } from '@/components/CommonPage';
 import { DatasourcesTable } from '@/components/datasources/DatasourcesTable';
-import AddDatasourceButton from '@/components/datasources/AddDatasourceButton';
+import AddDatasourceForm from '@/components/datasources/AddDatasourceForm';
 
 export function DatasourcesPage() {
     return (
         <CommonPage>
             <div className='flex items-center justify-between'>
                 <h1 className='text-3xl font-bold leading-tight'>Datasources</h1>
-                <AddDatasourceButton />
+                <AddDatasourceForm />
             </div>
 
             <div className='mt-5'>
