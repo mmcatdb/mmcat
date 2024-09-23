331ca01ccdb8c10373f24139816336fab88793e3
Šárka Mayerová <mayerova.s@email.cz>
2024-09-23 20:07:08 +0200

    React.FC to regular func component

--- client/src/components/datasources/DatasourcesTable.tsx
+++ client/src/components/datasources/DatasourcesTable.tsx
@@ -21,9 +21,11 @@ export const DatasourcesTable = () => {
                     setDatasources(response.data);
                 else
                     setError('Failed to load data');
-            } catch (err) {
+            }
+            catch (err) {
                 setError('Failed to load data');
-            } finally {
+            } 
+            finally {
                 setLoading(false);
             }
         };
@@ -31,11 +33,10 @@ export const DatasourcesTable = () => {
         fetchDatasources();
     }, []);
     
-
     if (loading) {
         return (
             <div>
-                <Spinner/>
+                <Spinner />
             </div>
         );
     }
@@ -54,7 +55,9 @@ type DatasourceTableProps = {
     datasources: Datasource[];
 }
 
-const DatasourceTable: React.FC<DatasourceTableProps> = ({ datasources }) => {
+function DatasourceTable(props: DatasourceTableProps) {
+    const { datasources } = props;
+
     return (
         <Table aria-label='Example static collection table'>
             <TableHeader>
@@ -81,100 +84,6 @@ const DatasourceTable: React.FC<DatasourceTableProps> = ({ datasources }) => {
             </TableBody>
         </Table>
     );
-};
+}
 
-
-
-// import React from 'react';
-// import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Chip, Tooltip } from '@nextui-org/react';
-// import { EditIcon } from '@/components/icons/EditIcon';
-// import { DeleteIcon } from '@/components/icons/DeleteIcon';
-// import { EyeIcon } from '@/components/icons/EyeIcon';
-// import { columns, users } from './data';
-
-// // TODO: Add pagination and `Add New` Button
-// // https://nextui.org/docs/components/table#custom-styles
-
-// type User = {
-//   id: number;
-//   label: string;
-//   type: string;
-//   host: string;
-//   port: string;
-//   database: string;
-//   actions: string;
-// }
-
-// type ColumnKey = 'label' | 'id' | 'type' | 'actions';
-
-// export function DatabasesTable() {
-//     const renderCell = React.useCallback((user: User, columnKey: ColumnKey) => {
-//         const cellValue = user[columnKey];
-
-//         switch (columnKey) {
-//         case 'label':
-//             return (
-//                 <div className='flex flex-col'>
-//                     <p className='text-bold text-sm capitalize'>{cellValue}</p>
-//                 </div>
-//             );
-//         case 'id':
-//             return (
-//                 <div className='flex flex-col'>
-//                     <p className='text-bold text-sm capitalize'>{cellValue}</p>
-//                 </div>
-//             );
-//         case 'type':
-//             return (
-//                 <Chip className='capitalize' size='sm' variant='flat'>
-//                     {cellValue}
-//                 </Chip>
-//             );
-//         case 'actions':
-//             return (
-//                 <div className='relative flex items-center gap-2'>
-//                     <Tooltip content='Details'>
-//                         <span className='text-lg text-default-400 cursor-pointer active:opacity-50'>
-//                             <EyeIcon />
-//                         </span>
-//                     </Tooltip>
-//                     <Tooltip content='Edit user'>
-//                         <span className='text-lg text-default-400 cursor-pointer active:opacity-50'>
-//                             <EditIcon />
-//                         </span>
-//                     </Tooltip>
-//                     <Tooltip color='danger' content='Delete user'>
-//                         <span className='text-lg text-danger cursor-pointer active:opacity-50'>
-//                             <DeleteIcon />
-//                         </span>
-//                     </Tooltip>
-//                 </div>
-//             );
-//         default:
-//             return cellValue;
-//         }
-//     }, []);
-
-//     return (
-//         <Table removeWrapper aria-label='Project 2(NUMBER) details'>
-//             <TableHeader columns={columns}>
-//                 {(column) => (
-//                     <TableColumn key={column.uid}>
-//                         {column.name}
-//                     </TableColumn>
-//                 )}
-//             </TableHeader>
-//             <TableBody items={users}>
-//                 {(item) => (
-//                     <TableRow key={item.id}>
-//                         {columns.map((column) => (
-//                             <TableCell key={column.uid}>
-//                                 {renderCell(item, column.uid as ColumnKey)}
-//                             </TableCell>
-//                         ))}
-//                     </TableRow>
-//                 )}
-//             </TableBody>
-//         </Table>
-//     );
-// }
+export default DatasourcesTable;
