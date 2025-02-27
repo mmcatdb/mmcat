6c72a34040d7a745f96c7ac02c4b2502b055a73e
srutkoa <srutkoa@cuni.cz>
2025-02-27 08:01:59 +0100

    file object

---
+++ library/server/src/main/java/cz/matfyz/server/entity/file/File.java
@@ -0,0 +1,36 @@
+package cz.matfyz.server.entity.file;
+
+import cz.matfyz.server.entity.Entity;
+import cz.matfyz.server.entity.Id;
+
+import java.util.Date;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+/**
+ * Represents a generic file object that can store various types of data.
+ * However, primarily intended to hold the outputs of jobs.
+ */
+public class File extends Entity {
+
+    public enum FileType {
+        DML, DATA_FILE;
+    }
+
+    public @Nullable Id jobId;
+    public @Nullable Id datasourceId;
+    public final String filename;
+    public final Date createdAt;
+    public final FileType type;
+
+    private File(Id id, @Nullable Id jobId, @Nullable Id datasourceId, String filename, Date createdAt, FileType type) {
+        super(id);
+        this.jobId = jobId;
+        this.datasourceId = datasourceId;
+        this.filename = filename;
+        this.createdAt = createdAt;
+        this.type = type;
+    }
+
+
+}
