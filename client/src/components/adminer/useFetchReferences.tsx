4bd4f09ee5a89077844aa7663994b2b2f9bf528f
Klára Hubínková <klara.hubinkova@volny.cz>
2025-03-07 14:28:38 +0100

    Update useFetchReferences

--- client/src/components/adminer/useFetchReferences.tsx
+++ client/src/components/adminer/useFetchReferences.tsx
@@ -1,26 +1,162 @@
 import { useState, useEffect } from 'react';
+import { api } from '@/api';
+import { Signature } from '@/types/identifiers/Signature';
+import type { StaticNameFromServer } from '@/types/identifiers/Name';
+import type { SignatureIdFromServer } from '@/types/identifiers/SignatureId';
 import type { AdminerState } from '@/types/adminer/Reducer';
 import type { AdminerReferences } from '@/types/adminer/AdminerReferences';
-import { api } from '@/api';
+import type { ChildPropertyFromServer, RootPropertyFromServer, SimplePropertyFromServer } from '@/types/accessPath/serverTypes';
+import type { SchemaCategoryFromServer } from '@/types/schema';
+import type { MappingFromServer } from '@/types/mapping';
+import type { Id } from '@/types/id';
 
-export function useFetchReferences( state: AdminerState ) {
-    const [ references, setReferences ] = useState<AdminerReferences | undefined>();
+function getLastBase(signature: string): string {
+    const signatureFromServer: Signature = Signature.fromServer(signature);
+    const lastBase = signatureFromServer.getLastBase();
+    return lastBase!.last.toString();
+}
+
+function getPropertiesFromAccessPath(
+    accessPath: RootPropertyFromServer | ChildPropertyFromServer,
+    properties: Set<SimplePropertyFromServer>,
+): Set<SimplePropertyFromServer>{
+    if ('signature' in accessPath && accessPath.signature !== 'EMPTY')
+        properties.add({ name: accessPath.name, signature: getLastBase(accessPath.signature) });
+
+
+    if ('subpaths' in accessPath && Array.isArray(accessPath.subpaths)) {
+        for (const subpath of accessPath.subpaths)
+            getPropertiesFromAccessPath(subpath, properties);
+
+    }
+
+    return properties;
+}
+
+function addReferences(
+    references: AdminerReferences,
+    primaryKeys: SignatureIdFromServer,
+    targetMapping: MappingFromServer,
+    sourceMapping: MappingFromServer,
+    targetProperties: SimplePropertyFromServer[],
+    sourceProperties: SimplePropertyFromServer[],
+    primaryKeyInTarget: boolean,
+) {
+    for (const key of primaryKeys) {
+        const keyLastBase = getLastBase(key);
+
+        const keyPropertyValue = targetProperties
+            .filter(property => property.signature === keyLastBase && 'value' in property.name)
+            .map(property => (property.name as StaticNameFromServer).value)[0] || '';
+
+        sourceProperties
+            .filter(mappingProp => mappingProp.signature === keyLastBase && 'value' in mappingProp.name)
+            .forEach(mappingProp => {
+                const mappingPropertyValue = (mappingProp.name as StaticNameFromServer).value;
+                references.push({
+                    referencedDatasourceId: targetMapping.datasourceId,
+                    referencedKindName: targetMapping.kindName,
+                    referencedProperty: primaryKeyInTarget ? keyPropertyValue : mappingPropertyValue,
+                    referencingDatasourceId: sourceMapping.datasourceId,
+                    referencingKindName: sourceMapping.kindName,
+                    referencingProperty: primaryKeyInTarget ? mappingPropertyValue : keyPropertyValue,
+                });
+            });
+    }
+}
+
+function addMappingReferences(
+    references: AdminerReferences,
+    mapping: MappingFromServer,
+    kindMapping: MappingFromServer,
+    kindProperties: Set<SimplePropertyFromServer>,
+): AdminerReferences {
+    const mappingPathProperties = getPropertiesFromAccessPath(mapping.accessPath, new Set<SimplePropertyFromServer>());
+    const mappingPropertiesArray = Array.from(mappingPathProperties);
+    const kindPropertiesArray = Array.from(kindProperties);
+
+    addReferences(references, kindMapping.primaryKey, kindMapping, mapping, kindPropertiesArray, mappingPropertiesArray, true);
+
+    addReferences(references, mapping.primaryKey, mapping, kindMapping, mappingPropertiesArray, kindPropertiesArray, false);
+
+    return references;
+}
+
+async function getAllMappings(): Promise<MappingFromServer[]> {
+    const mappingResponse = await api.mappings.getAllMappings({});
+
+    if (!mappingResponse.status)
+        throw new Error(`Failed to fetch mappings`);
+
+    return mappingResponse.data;
+}
+
+async function getSchemaCategory(categoryId: string): Promise<SchemaCategoryFromServer> {
+    const schemaCategoryResponse = await api.schemas.getCategory({ id: categoryId });
+
+    if (!schemaCategoryResponse.status)
+        throw new Error(`Failed to fetch schema categories`);
+
+    return schemaCategoryResponse.data;
+}
+
+async function getSchemaCategoryReferences(datasourceId: Id, kindName: string): Promise<AdminerReferences> {
+    const mappings = await getAllMappings();
+
+    const kindMappings = mappings.filter(mapping =>
+        mapping.kindName === kindName &&
+        mapping.datasourceId === datasourceId);
+
+    if (kindMappings.length === 0)
+        return [];
+
+    const kindMapping = kindMappings[0];
+
+    const schemaCategory = await getSchemaCategory(kindMapping.categoryId);
+
+    const schemaCategoryMappings = mappings.filter(mapping =>
+        mapping.categoryId === schemaCategory.id );
+
+    const mappingPathProperties = getPropertiesFromAccessPath(kindMapping.accessPath, new Set<SimplePropertyFromServer>());
+
+    const references: AdminerReferences = [];
+
+    for (const mapping of schemaCategoryMappings)
+        addMappingReferences(references, mapping, kindMapping, mappingPathProperties);
+
+    return references;
+}
+
+async function getForeignKeys(datasourceId: Id, kindName: string): Promise<AdminerReferences> {
+    const foreignKeysResponse = await api.adminer.getReferences({ datasourceId: datasourceId, kindName: kindName });
+
+    if (!foreignKeysResponse.status)
+        throw new Error(`Failed to fetch foreign keys`);
+
+    return foreignKeysResponse.data;
+}
+
+export function useFetchReferences(state: AdminerState) {
     const [ loading, setLoading ] = useState<boolean>(true);
+    const [ references, setReferences ] = useState<AdminerReferences>([]);
 
     useEffect(() => {
         (async () => {
             setLoading(true);
 
-            const response = await api.adminer.getReferences({ datasourceId: state.datasourceId!, kindName: state.kindName! });
+            try {
+                const foreignKeys = await getForeignKeys(state.datasourceId!, state.kindName!);
+                const schemaCategoryReferences = await getSchemaCategoryReferences(state.datasourceId!, state.kindName!);
 
-            if (!response.status)
-                setReferences(undefined);
-            else
-                setReferences(response.data);
+                setReferences(foreignKeys.concat(schemaCategoryReferences));
+            }
+            catch (e) {
+                console.log(e);
+            }
 
             setLoading(false);
         })();
     }, [ state.datasourceId, state.kindName ]);
 
-    return { references, referencesLoading: loading };
+    return { references: references, referencesLoading: loading };
 }
