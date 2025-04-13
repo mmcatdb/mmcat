91eee2e1f059053de5d67a6df398e1954e7eec7c
Klára Hubínková <klara.hubinkova@volny.cz>
2025-04-13 17:32:49 +0200

    Update database view

--- client/src/components/adminer/DatabaseGraph.tsx
+++ client/src/components/adminer/DatabaseGraph.tsx
@@ -81,16 +81,19 @@ export function DatabaseGraph({ fetchedData, kind }: DatabaseTableProps ) {
     }, [ fetchedData ]);
 
     return (
-        <div className='max-w-[1280px] text-center h-[400px]'>
-            {fetchedData && (
-                <ArcThemeProvider theme={'dark'}>
-                    <GraphVisualizer
-                        nodes={nodes}
-                        relationships={relationships}
-                    />
-                </ArcThemeProvider>
-            )
-            }
+        <div>
+            {fetchedData && fetchedData.data.length > 0 ? (
+                <div className='max-w-[1280px] text-center h-[400px]'>
+                    <ArcThemeProvider theme={'dark'}>
+                        <GraphVisualizer
+                            nodes={nodes}
+                            relationships={relationships}
+                        />
+                    </ArcThemeProvider>
+                </div>
+            ) : (
+                <span>No records to display.</span>
+            )}
         </div>
     );
 }
