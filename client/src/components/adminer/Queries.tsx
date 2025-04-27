9d21460961c06303b5a7d98930b34ac7ad3b5070
Klára Hubínková <klara.hubinkova@volny.cz>
2025-04-27 21:55:53 +0200

    Update queries

--- client/src/components/adminer/Queries.tsx
+++ client/src/components/adminer/Queries.tsx
@@ -1,9 +1,9 @@
 import { DatasourceType } from '@/types/datasource';
 
 export const EXAMPLE_QUERY: Record<DatasourceType, string> = {
-    [DatasourceType.neo4j]: 'MATCH (n) RETURN n;',
-    [DatasourceType.mongodb]: '{ find: \'address\' }',
-    [DatasourceType.postgresql]: 'SELECT * FROM "order";',
+    [DatasourceType.neo4j]: 'MATCH (u)-[:FRIEND]->(f)-[:FRIEND]->(fof) WHERE f.id = \'user_005\' RETURN u, f, fof;',
+    [DatasourceType.mongodb]: '{"find": "business", "filter": {"attributes.wifi": "free"}}',
+    [DatasourceType.postgresql]: 'SELECT u.name, u.fans, c.business_id, c.date, c.text, c.stars FROM "user" u JOIN "comment" c ON (u.user_id = c.user_id) WHERE c.stars >= 2;',
     [DatasourceType.csv]: '',
     [DatasourceType.json]: '',
     [DatasourceType.jsonld]: '',
