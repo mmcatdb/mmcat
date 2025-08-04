ad67fb243b3d72c83754b46362de0fe7a5e4bcc6
Filip Štrobl <filip.strobl@seznam.cz>
2025-08-04 18:07:33 +0200

    Fix improper Nullable import

--- library/inference/src/main/java/cz/matfyz/inference/edit/InferenceEditorUtils.java
+++ library/inference/src/main/java/cz/matfyz/inference/edit/InferenceEditorUtils.java
@@ -19,7 +19,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
-import javax.annotation.Nullable;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 import org.apache.hadoop.yarn.webapp.NotFoundException;
 
