7fe1ed43ada40a756a3e91867c21aff5cd3f0ff3
Filip Štrobl <filip.strobl@seznam.cz>
2026-01-15 11:59:45 +0100

    Add switchable optimization options

--- library/querying/src/main/java/cz/matfyz/querying/optimizer/QueryOptimizer.java
+++ library/querying/src/main/java/cz/matfyz/querying/optimizer/QueryOptimizer.java
@@ -1,16 +1,30 @@
 package cz.matfyz.querying.optimizer;
 
+import cz.matfyz.core.utils.Config;
 import cz.matfyz.querying.planner.QueryPlan;
 
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 public class QueryOptimizer {
 
+    // Stuff in this class
+    static boolean predicatePushdown = Config.GLOBAL.getBool("optimization.predicatePushdown");
+    static boolean dependentJoins = Config.GLOBAL.getBool("optimization.dependentJoins");
+
+    // Stuff in other classes (due to the query pipeline structure they cannot be put here)
+    static boolean neo4jMatchConcatenation = Config.GLOBAL.getBool("optimization.neo4jMatchConcatenation");
+    static boolean joinPlanSelection = Config.GLOBAL.getBool("optimization.joinPlanSelection");
+
+
     public static QueryPlan run(QueryPlan queryPlan, @Nullable CollectorCache cache) {
 
-        FilterDeepener.run(queryPlan);
+        if (predicatePushdown) {
+            FilterDeepener.run(queryPlan);
+        }
 
-        if (cache != null) JoinDependator.run(queryPlan, cache);
+        if (dependentJoins && cache != null) {
+            JoinDependator.run(queryPlan, cache);
+        }
 
         // TODO Other optimization techniques:
         // - Split filters to further deepen them (WARNING: reference nodes and filtered nodes may change, some checks are required)
