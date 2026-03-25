cb21dcb94c251c19830aa2ffa2ac25963b6e1626
tuaki <jachymb32@gmail.com>
2026-03-25 08:13:58 +0100

    fix4

--- library/docker/prod/entrypoint.sh
+++ library/docker/prod/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# TODO Not done automatically - do it yourself
 cd aimm
 python3 -m venv venv
 source .venv/bin/activate
 pip install -e .
 
-java -jar server/target/server-1.0-SNAPSHOT-app.jar
+# java -jar server/target/server-1.0-SNAPSHOT-app.jar
